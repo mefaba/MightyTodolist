5d267ba0e8c676b5c7c2a896165edc91bb2d3b25
mefa <mfbyrktr@gmail.com>
2020-05-15 12:14:34 +0300

    Small Dragging Effect without Drop Function

--- src/components/todo-list/Todo-List.unit.js
+++ src/components/todo-list/Todo-List.unit.js
@@ -2,34 +2,31 @@ import React from "react";
 import TodoItem from "../todo-item/Todo-Item.unit";
 import { useSelector } from "react-redux";
 import { useRef } from "react";
-import { motion } from "framer-motion";
+import { motion} from "framer-motion";
 import "./todo-list.css";
 
 const TodoList = () => {
 	const todolist = useSelector((state) => state.list.todolist);
     const constraintsRef = useRef(null);
-    console.log(todolist)
     //oldindex
     //newindex
-    const todolist_test = ["Erdem","Fatih", "Murat", "Aytu"]
 	//TODOLİST WİLL BE CONTSTRAİNT AREA
     //TODO İTEMS WİLL BE DRAGGABLE
-    const HandleChange = (e)=> {
-        console.log(e)
-    }
-	return (
-		<div className="todo-list" ref={constraintsRef}>
-			{todolist.map((todoitemX, index) => {
-				return (
+
+    return (
+        <div className="todo-list" ref={constraintsRef}>
+            {todolist.map((todoitemX, index) => {
+                return (
                     <motion.div 
                     key={todoitemX.id} 
                     drag="y"
-                    onChange={(e)=>HandleChange()}
-                    dragConstraints={constraintsRef}>
-						<TodoItem  todoitem={todoitemX.text} id={todoitemX.id} />
-					</motion.div>
-				);
-			})}
+                    dragConstraints={{top:0, bottom:0}}
+                    dragElastic={0.05}
+                    >
+                        <TodoItem  todoitem={todoitemX.text} id={todoitemX.id} />
+                    </motion.div>
+                );
+            })}
         </div>
     )
 };
@@ -44,3 +41,5 @@ export default TodoList;
         userEmail: state.user.userEmail
     }
   } */
+
+  //STABLE 
